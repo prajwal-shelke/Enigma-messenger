16f3c0d40ccffa8363a7e016d89e7549741446a3
prajwal-shelke <72300787+prajwal-shelke@users.noreply.github.com>
2021-08-05 14:54:08 +0530

    update

--- php/users.php
+++ php/users.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include_once "https://enigma-messenger.herokuapp.com/config.php";
+    include_once "config.php";
     $outgoing_id = $_SESSION['unique_id'];
     $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} ORDER BY user_id DESC";
     $query = mysqli_query($conn, $sql);
@@ -8,7 +8,7 @@
     if(mysqli_num_rows($query) == 0){
         $output .= "No users are available to chat";
     }elseif(mysqli_num_rows($query) > 0){
-        include_once "https://enigma-messenger.herokuapp.com/data.php";
+        include_once "data.php";
     }
     echo $output;
 ?>
